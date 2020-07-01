fc1d4ef9609672caf38394b8737b6c00dc931fb2
jonathan200934 <jonathan200934@gmail.com>
2020-07-01 19:06:55 +0800

    second

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,get_object_or_404,redirect
 from django.utils import timezone
 from blog.models import Post,Comments
 from blog.forms import PostForm,CommentForm
-from django.urls import reverse_lazy
+from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.views.generic import (TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView)
