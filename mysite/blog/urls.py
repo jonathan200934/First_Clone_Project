18d6a1efe3491dc31a352d44183de77e76c0c5de
Jonathan <jonathan200934@gmail.com>
2020-07-06 17:33:00 +0800

    added TicTacToe

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
     path('connectfour/',views.ConnectFourView.as_view(),name='connectfour'),
+    path('tictactoe/',views.TicTacToeViews.as_view(),name='tictactoe'),
 ]
