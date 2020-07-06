0f101cfc05c96df2007248f092c8920c95dceb51
Jonathan <jonathan200934@gmail.com>
2020-07-06 15:07:36 +0800

    fixed ConnectFour's MIME issue

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -22,5 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
-    path('connectfour/',include('ConnectFour.urls'))
 ]
